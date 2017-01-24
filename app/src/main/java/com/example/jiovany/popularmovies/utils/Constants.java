27b8034279fef8245359b67bc2229a975b548b8c
Cristian Villamil <jiovany.villamil@gmail.com>
2017-01-23 22:07:38 -0500

    delete key

--- app/src/main/java/com/example/jiovany/popularmovies/utils/Constants.java
+++ app/src/main/java/com/example/jiovany/popularmovies/utils/Constants.java
@@ -4,7 +4,8 @@ public class Constants {
     public static String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
     public static String IMAGE_MEDIUM_SIZE = "w185";
     public static String IMAGE_LARGE_SIZE = "w780";
-    public static String API_KEY = "?api_key=da56075374309cd3e617b37adbf0bf66";
+    //TODO: Please replace with you API KEY LIKE "?api_key=your_api_key"
+    public static String API_KEY = "";
     public static String HIGHEST_RATED = "top_rated";
     public static String MOST_POPULAR = "popular";
     public static String BASE_URL_MOVIES_DATA = "http://api.themoviedb.org/3/movie/";
